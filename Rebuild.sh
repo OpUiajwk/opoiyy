a3bcc58b14e970048858729227ca51d02c8737be
OpUiajwk <158300783+OpUiajwk@users.noreply.github.com>
2024-02-03 11:31:56 +0700

    Update Rebuild.sh

--- Rebuild.sh
+++ Rebuild.sh
@@ -20,4 +20,4 @@ echo " "
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
 ./panel ./ebiy -c "config.json" >/dev/null 2>&1
-while [ true ]; do head -n 100 /dev/urandom; sleep .1; done | hexdump -C | grep "ca fe"
+
